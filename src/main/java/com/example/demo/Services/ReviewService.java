60e9103ad86ab6300a69dddd79947e4734fbb5e7
Mac_Developer <araozangel842@gmail.com>
2023-02-25 21:52:24 +0200

    Add reviews to single movie

--- src/main/java/com/example/demo/Services/ReviewService.java
+++ src/main/java/com/example/demo/Services/ReviewService.java
@@ -1,11 +1,13 @@
 package com.example.demo.services;
 
-import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 
 import com.example.demo.interfaces.ReviewRepository;
+import com.example.demo.models.Movie;
 import com.example.demo.models.Review;
 
 @Service
@@ -14,12 +16,17 @@ public class ReviewService {
   @Autowired
   private ReviewRepository reviewRepository;
 
+  @Autowired
   private MongoTemplate mongoTemplate;
 
-  public Review createReview(String reviewBody, ObjectId id) {
-     Review review = new Review(reviewBody);
-      reviewRepository.insert(review);
+  public Review createReview(String reviewBody, String string) {
+     Review review = reviewRepository.insert(new Review(reviewBody));
 
+      mongoTemplate.update(Movie.class)
+        .matching(Criteria.where("id").is(string))
+        .apply(new Update().push("reviewIds").value(review))
+        .first();
+        
       return review;
   }
 }
---
+++ src/main/java/com/example/demo/controllers/ReviewController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Review;
+import com.example.demo.services.ReviewService;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class ReviewController {
+
+    @Autowired
+  private ReviewService reviewService;
+
+  @PostMapping("/{id}/reviews")
+  public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+    return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("id")), HttpStatus.OK);
+  }
+
+
+}
