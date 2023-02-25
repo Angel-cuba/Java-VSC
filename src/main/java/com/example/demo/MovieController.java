93cacbb3512a86d37648f1ac32854251f6ad357a
Mac_Developer <araozangel842@gmail.com>
2023-02-25 12:07:54 +0200

    Get data by id

--- src/main/java/com/example/demo/MovieController.java
+++ src/main/java/com/example/demo/MovieController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +27,10 @@ public class MovieController {
   public ResponseEntity<List<Movie>> getMovies() {
     // return ResponseEntity.ok("Hello World from my Java app!");
     return new ResponseEntity<List<Movie>>(movieService.getMovies(), HttpStatus.OK);
+  }
 
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id) {
+    return new ResponseEntity<Optional<Movie>>(movieService.getMovieById(id), HttpStatus.OK);
   }
 }
--- src/main/java/com/example/demo/Services/MovieService.java
+++ src/main/java/com/example/demo/Services/MovieService.java
@@ -1,7 +1,9 @@
 package com.example.demo.Services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,12 @@ import com.example.demo.Interfaces.MovieRepository;
 public class MovieService {
   @Autowired
   private MovieRepository movieRepository;
+
   public List<Movie> getMovies() {
     return movieRepository.findAll();
+  }
+
+  public Optional<Movie> getMovieById(ObjectId id) {
+    return movieRepository.findById(id);
   }
 }
