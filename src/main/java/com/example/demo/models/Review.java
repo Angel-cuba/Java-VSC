46b5845042418f7de00b126ca9f93e2d47a0224d
Mac_Developer <araozangel842@gmail.com>
2023-02-27 16:56:56 +0200

    Refactoring and adding lombok props

--- src/main/java/com/example/demo/models/Review.java
+++ src/main/java/com/example/demo/models/Review.java
@@ -2,24 +2,22 @@ package com.example.demo.models;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
   @Id
     private ObjectId id;
     private String body;
 
-    public ObjectId getId() {
-      return id;
+    public Review(String body){
+        this.body = body;
     }
-    public void setId(ObjectId id) {
-      this.id = id;
-    }
-    public String getBody() {
-      return body;
-    }
-    public Review(String body) {
-      this.body = body;
-    }
-
 }
