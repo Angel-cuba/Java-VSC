fcaf6a7327516e423f0e67f6aad3a429a63c1620
Mac_Developer <araozangel842@gmail.com>
2023-03-10 13:24:17 +0200

    Add method to find user by email address

--- src/main/java/com/example/demo/Interfaces/UserRepository.java
+++ src/main/java/com/example/demo/Interfaces/UserRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.interfaces;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.demo.models.User;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
-  
+  Optional<User> findUserByEmail(String email);
 }
