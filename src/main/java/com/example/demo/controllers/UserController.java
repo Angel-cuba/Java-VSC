eae5c9106f3188ee2ba88728c878120616be0b49
Mac_Developer <araozangel842@gmail.com>
2023-03-10 13:25:39 +0200

    Add controllers to handle user methods

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,8 +1,53 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.models.User;
+import com.example.demo.services.UserService;
+
+import jakarta.websocket.server.PathParam;
 
 @Controller
+@RequestMapping("/api/v1/users")
 public class UserController {
   
+  @Autowired
+  private UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<User>> getAllUsers() {
+    return new ResponseEntity<List<User>>(userService.getUsers(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<User> getUserById(@PathVariable ObjectId id) {
+    return userService.getUserById(id);
+  }
+
+  @GetMapping("/email")
+  public ResponseEntity<User> getUserByEmail(@PathParam("email") String email) {
+    return userService.getUserByEmail(email); 
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<User> createUser(@RequestBody User user) {
+    return userService.createUser(user);
+  }
+
+
 }
